60c38b9761ab4e119403a1dacdf03e4c9852b5d6
BeanBooy <felix.pannenb@gmail.com>
2021-12-29 13:47:37 +0100

    new files

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+// js
+const cursor = document.getElementById("cursor");
 
+document.addEventListener("mousemove",(e) => {
+	//console.log("asdf");
+	document.getElementById("cursor").style.left = e.pageX + 'px';
+	document.getElementById("cursor").style.top =  e.pageY + 'px';
+	document.getElementById("cursor2").style.left = e.pageX + 'px';
+	document.getElementById("cursor2").style.top =  e.pageY + 'px';
+} )
