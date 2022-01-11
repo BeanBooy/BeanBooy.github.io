a1ab33039cbdf3f5f868e2d092da21063977f588
BeanBooy <felix.pannenb@gmail.com>
2022-01-11 14:02:12 +0100

    header update

--- script.js
+++ script.js
@@ -28,16 +28,21 @@ var wheelRotation = [0, 0, 0];
 
 function spin() {
 	
+	console.log("-------------------------");
+	
 	for (let i = 0; i < wheelRotation.length; i++) {
 		
 		var elementsRolled = getRandomInt(20, 50);
 		console.log(elementsRolled);
+		var onElementId = elementsRolled % 9;
+		console.log("Wheel on Element " + onElementId);
 		var degAdded = elementsRolled * 40;
 		wheelRotation[i] += degAdded;
 		document.getElementById("wheel_" + i).style.transitionDuration  = elementsRolled * 0.1 + "s";
 		document.getElementById("wheel_" + i).style.transform = "rotateY(" + wheelRotation[i] + "deg)";
 	}
 	
+	console.log("-------------------------");
 }
 
 
