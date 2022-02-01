a5dcf951ca1b774189334e08d50d55328b7a1b23
BeanBooy <96286172+BeanBooy@users.noreply.github.com>
2022-02-01 08:40:31 +0100

    Update script.js
    random int

--- script.js
+++ script.js
@@ -47,3 +47,9 @@ function spin() {
 	
 	console.log("-------------------------");
 }
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
